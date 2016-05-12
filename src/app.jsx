63af87f2f35f48540b1fe36ac08f990256011b0b
adam.gajzlerowicz <nelf86@gmail.com>
2016-05-12 21:27:09 +0100

    added ajax

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,37 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var request = require('request');
 
 var Hello = React.createClass({
-  render: function() {
-    return <h1 className="red">
-      Hello!
-    </h1>
-  }
+
+    getInitialState: function() {
+        return {
+            username: 'adam',
+            data: {}
+        };
+    },
+
+    componentDidMount: function() {
+        request(this.props.source, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var json = JSON.parse(body);
+                this.setState({
+                    data: json
+                });
+w            }
+        }.bind(this));
+    },
+
+    render: function() {
+
+        return <h1 className="red">
+            Hello {this.state.username}!
+        {this.state.data.foo}
+        </h1>
+    }
 });
 
-var element = React.createElement(Hello, {});
-ReactDOM.render(element, document.querySelector('.container'));
+ReactDOM.render(
+    <Hello source="http://localhost/api.php" />,
+    document.querySelector('.container')
+);
